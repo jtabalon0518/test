fc034658ab4ca94b14af0afbc9dfd311fee2659c
jtabalon0518 <jtabalon0518@sdsu.edu>
2025-09-03 14:47:44 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 int main() {
-    std::cout << "Mission Control is ready!" << std::endl;
+
+    int num = 10;
+
+    if (num > 0) {
+        cout <<"Number is positive" << endl;
+    } else if (num < 0) {
+        cout <<"Number is negative" << endl;
+    } else {
+        cout << "Number is zero" << endl;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        cout << "i is "<<i<<endl;
+    }
+    int i = 0;
+
     return 0;
 }
